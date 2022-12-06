df73ec82401cf35d60c6c42df98268257f2ec254
Muzaffar1908 <muzaffarahmadaliyev99@gmail.com>
2022-12-06 15:49:18 +0500

    livewire

---
+++ resources/views/admin/layout/partials/sidebar.blade.php
@@ -0,0 +1,87 @@
+<div class="deznav">
+    <div class="deznav-scroll">
+        <ul class="metismenu" id="menu">
+            <li class="">
+                <a class="has-arrow ai-icon" href="{{route('index')}}" aria-expanded="false">
+                    <i class="flaticon-381-networking"></i>
+                    <span class="nav-text">Dashboard</span>
+                </a>
+            </li>
+            <li class="has-menu"><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-381-networking"></i>
+                    <span class="nav-text">Users</span>
+               </a>
+                <ul aria-expanded="false">
+                    <li><a href="{{route('u-index')}}">Users</a></li>
+                </ul>
+            </li>
+            <li class="has-menu">
+                <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-381-networking"></i>
+                    <span class="nav-text">News</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="{{route('c-index')}}">News Category</a></li>
+                    <li><a href="{{route('n-index')}}">News</a></li>
+                </ul>
+            </li>
+            <li class="has-menu">
+                <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-381-networking"></i>
+                    <span class="nav-text">Main Page</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="{{route('m-index')}}">Main Page</a></li>
+                </ul>
+            </li>
+            <li class="has-menu">
+                <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-381-networking"></i>
+                    <span class="nav-text">About</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="{{route('a-index')}}">About</a></li>
+                </ul>
+            </li>
+            <li class="has-menu">
+                <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-381-networking"></i>
+                    <span class="nav-text">Partner</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="{{route('p-index')}}">Partner</a></li>
+                </ul>
+            </li>
+            <li class="has-menu">
+                <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-381-networking"></i>
+                    <span class="nav-text">Contact</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="{{route('ct-index')}}">Contact</a></li>
+                </ul>
+            </li>
+            <li class="has-menu">
+                <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-381-networking"></i>
+                    <span class="nav-text">Gallery</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="{{route('g-index')}}">Gallery</a></li>
+                </ul>
+            </li>
+            <li class="has-menu">
+                <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-381-networking"></i>
+                    <span class="nav-text">National Office</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="{{route('d-index')}}">Divisions</a></li>
+                    <li><a href="{{route('po-index')}}">Positions</a></li>
+                    <li><a href="{{route('emp-index')}}">Employee</a></li>
+                </ul>
+            </li>
+        </ul>
+        
+    </div>
+</div>

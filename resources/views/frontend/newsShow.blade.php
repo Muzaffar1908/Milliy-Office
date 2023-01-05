7a216fa014cd9d73c6b970498e1dd593001a731b
Muzaffar1908 <muzaffarahmadaliyev99@gmail.com>
2023-01-05 17:04:39 +0500

    newshsiw

--- resources/views/frontend/newsShow.blade.php
+++ resources/views/frontend/newsShow.blade.php
@@ -63,6 +63,23 @@
                                 @endforeach
                             </ul>
                         </div>
+                        <div class="blog-widget mt-5">
+                            <h4 class="blog-widget-title">Social Media</h4>
+                            <ul class="social-three">
+                                <li>
+                                  <a href="https://www.instagram.com/"><i class='bx bxl-linkedin'></i></a>
+                                </li>
+                                <li>
+                                  <a href="https://www.dribbble.com/"><i class='bx bxl-pinterest-alt'></i></a>
+                                </li>
+                                <li>
+                                  <a href="https://www.pinterest.com/"><i class='bx bxl-instagram'></i></a>
+                                </li>
+                                <li>
+                                  <a href="https://www.twitter.com/"><i class='bx bxl-facebook'></i></a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
